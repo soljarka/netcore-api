3c77cfe0c78ab4a558f15e4152ba462926abc9ef
Andrey Strizhevsky <Andrey_Strizhevsky@rcomext.com>
2018-10-03 02:34:24 +0300

    add cars endpoint

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace netcore_api.Controllers
@@ -14,15 +16,14 @@ namespace netcore_api.Controllers
     public class ValuesController : ControllerBase
     {
         private string beerApi = "https://go-for-beer.azurewebsites.net/secured/beers";
+        private string carsApi = "https://oauth2-server.ac-np.swissre.com/cars";
 
         // GET api/values
         [Authorize]
         [HttpGet("protected")]
         public async Task<ActionResult<string>> Protected()
         {
-            var header = HttpContext.Request.Headers["Authorization"];
-            var headerArr = header[0].Split(new[] { ' ' });
-            var apiAddress = System.Environment.GetEnvironmentVariable("REMOTE_API_URL");
+            var apiAddress = System.Environment.GetEnvironmentVariable("BEER_API_URL");
             if(string.IsNullOrWhiteSpace(apiAddress))
             {
                 apiAddress = beerApi;
@@ -30,7 +31,7 @@ namespace netcore_api.Controllers
             using (var client = new HttpClient())
             {
                 var request = new HttpRequestMessage();
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(headerArr[0], headerArr[1]);
+                request.Headers.Authorization = GetAuthHeader(HttpContext.Request);
                 request.RequestUri = new Uri(apiAddress);
                 request.Method = HttpMethod.Get;
                 try
@@ -52,7 +53,46 @@ namespace netcore_api.Controllers
         [HttpGet("public")]
         public ActionResult<IEnumerable<string>> Public()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
+        }
+
+        [Authorize]
+        [HttpGet("cars")]
+        public async Task<ActionResult<string>> Cars()
+        {
+            var apiAddress = System.Environment.GetEnvironmentVariable("CARS_API_URL");
+            if (string.IsNullOrWhiteSpace(apiAddress))
+            {
+                apiAddress = carsApi;
+            }
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                request.Headers.Authorization = GetAuthHeader(HttpContext.Request);
+                request.RequestUri = new Uri(apiAddress);
+                request.Method = HttpMethod.Get;
+                try
+                {
+                    var response = await client.SendAsync(request);
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        return Unauthorized();
+                    }
+                    var message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+        }
+
+        private AuthenticationHeaderValue GetAuthHeader(HttpRequest request)
+        {
+            var header = request.Headers["Authorization"];
+            var headerArr = header[0].Split(new[] { ' ' });
+            return new AuthenticationHeaderValue("Bearer", headerArr[1]);
         }
 
     }

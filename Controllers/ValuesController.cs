a2ada786d6988102111819fea50ade73414e4195
Andrey Strizhevsky <Andrey_Strizhevsky@rcomext.com>
2018-10-03 03:04:22 +0300

    add amazonbeer

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -16,12 +16,14 @@ namespace netcore_api.Controllers
     public class ValuesController : ControllerBase
     {
         private string beerApi = "https://go-for-beer.azurewebsites.net/secured/beers";
+        private string amazonBeerApi = "https://go-for-beer.azurewebsites.net/secured/amazon-beers";
         private string carsApi = "https://oauth2-server.ac-np.swissre.com/cars";
+        private string userApi = "https://go-for-beer.azurewebsites.net/user";
 
         // GET api/values
         [Authorize]
-        [HttpGet("protected")]
-        public async Task<ActionResult<string>> Protected()
+        [HttpGet("beer")]
+        public async Task<ActionResult<string>> Beer()
         {
             var apiAddress = System.Environment.GetEnvironmentVariable("BEER_API_URL");
             if(string.IsNullOrWhiteSpace(apiAddress))
@@ -37,13 +39,78 @@ namespace netcore_api.Controllers
                 try
                 {
                     var response = await client.SendAsync(request);
-                    if(response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    if(response.StatusCode == HttpStatusCode.Unauthorized)
                     {
                         return Unauthorized();
                     }
                     var message = await response.Content.ReadAsStringAsync();
                     return message;
                 } catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+        }
+
+        // GET api/values
+        [Authorize]
+        [HttpGet("amazonbeer")]
+        public async Task<ActionResult<string>> AmazonBeer()
+        {
+            var apiAddress = System.Environment.GetEnvironmentVariable("AMAZON_BEER_API_URL");
+            if (string.IsNullOrWhiteSpace(apiAddress))
+            {
+                apiAddress = amazonBeerApi;
+            }
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                request.Headers.Authorization = GetAuthHeader(HttpContext.Request);
+                request.RequestUri = new Uri(apiAddress);
+                request.Method = HttpMethod.Get;
+                try
+                {
+                    var response = await client.SendAsync(request);
+                    if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        return Unauthorized();
+                    }
+                    var message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                catch (Exception ex)
+                {
+                    return ex.ToString();
+                }
+            }
+        }
+
+        [Authorize]
+        [HttpGet("user")]
+        public async Task<ActionResult<string>> User()
+        {
+            var apiAddress = System.Environment.GetEnvironmentVariable("USER_API_URL");
+            if (string.IsNullOrWhiteSpace(apiAddress))
+            {
+                apiAddress = userApi;
+            }
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                request.Headers.Authorization = GetAuthHeader(HttpContext.Request);
+                request.RequestUri = new Uri(apiAddress);
+                request.Method = HttpMethod.Get;
+                try
+                {
+                    var response = await client.SendAsync(request);
+                    if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        return Unauthorized();
+                    }
+                    var message = await response.Content.ReadAsStringAsync();
+                    return message;
+                }
+                catch (Exception ex)
                 {
                     return ex.ToString();
                 }
